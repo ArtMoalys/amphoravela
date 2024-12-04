c89a3689e1fe508b898049c4d1789b17d00424f0
ArtMoalys <161700977+ArtMoalys@users.noreply.github.com>
2024-12-04 21:31:25 +0300

    Update animationCard.js

--- script/animationCard.js
+++ script/animationCard.js
@@ -8,7 +8,7 @@ function modalTransformationAnimation(card) {
         if(event.target.classList.contains("cross")) {
             action = true;
             cross.style.display = "none";
-            card.style.height = `600px`;
+            card.style.height = `630px`;
             card.querySelector(".card__desc").innerHTML = '';
             document.querySelector("body").style.overflow = "visible";
 
